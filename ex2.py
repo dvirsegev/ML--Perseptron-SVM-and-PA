75c764e6689bac9e2672af4d2a1e45d7f8062f2a
Dvir Segev <dvirsegev@gmail.com>
2019-05-13 12:26:38 +0300

    finish

---
+++ ex2.py
@@ -0,0 +1,214 @@
+import sys
+from random import shuffle
+import numpy as np
+
+
+class Algorithm(object):
+
+    def file_len(fname):
+        with open(fname) as f:
+            for i, l in enumerate(f):
+                pass
+        return i + 1
+
+    def switch(x,listresult):
+        if (x == 'M'):
+            return listresult[x]
+        if (x == 'F'):
+            return listresult[x]
+        if (x == 'I'):
+            return listresult[x]
+
+    def checkRow(tmp):
+        generalcount=0
+        Mcount=0
+        Fcount=0
+        Icount=0
+        for input in range (tmp.shape[0]):
+            input = tmp[input][0]
+            if (input == 'M'):
+                Mcount+=1
+            if (input == 'F'):
+                Fcount+=1
+            if (input == 'I'):
+                Icount += 1
+            generalcount+=1
+        listresult={
+            "M": Mcount/generalcount,
+             "F": Fcount/generalcount,
+            "I": Icount / generalcount,
+        }
+        return listresult
+    def printtothefile(file, text):
+        file.write(text)
+
+    def test(self, inputext,w):
+        x = []
+        tmp = np.loadtxt(inputext, dtype='str', delimiter=',')
+        listresult = Algorithm.checkRow(tmp)
+        # switch the letter of the gender.
+        for array in range(tmp.shape[0]):
+            tmp[array][0] = Algorithm.switch(tmp[array][0], listresult)
+        inputarray = tmp.astype(float)
+        inputarray = (inputarray - np.mean(inputarray, axis=0)) / np.std(
+            inputarray, axis=0)
+        for inputs in inputarray:
+            y_hat = np.argmax(np.dot(w, inputs))
+            x.append(y_hat)
+        return x
+
+
+class Perceptron(Algorithm):
+
+    # initilizae the examples.
+    def setup(self, train_x, train_y):
+        tmp = np.loadtxt(train_x, dtype='str', delimiter=',')
+        listresult = Algorithm.checkRow(tmp)
+        # switch the letter of the gender.
+        for x in range(tmp.shape[0]):
+            tmp[x][0] = Algorithm.switch(tmp[x][0],listresult)
+        self.training_inputs = tmp.astype(float)
+        self.training_inputs = (self.training_inputs - np.mean(self.training_inputs, axis=0)) / np.std(
+            self.training_inputs, axis=0)
+        self.labels = np.loadtxt(train_y, dtype=int)
+        # shuffle examples
+        c = list(zip(self.training_inputs, self.labels))
+        shuffle(c)
+        self.training_inputs, self.labels = zip(*c)
+        x = np.max(self.labels[0])
+        # set width
+        self.w = np.zeros((3, 8), dtype=float)
+        self.learning_rate = 0.1
+
+    def train(self):
+        count=0
+        for set in range(100):
+            for inputs, label in zip(self.training_inputs, self.labels):
+                y_hat = np.argmax(np.dot(self.w, inputs))
+                if (y_hat != label):
+                    self.w[label, :] += self.learning_rate * inputs
+                    self.w[y_hat, :] -= self.learning_rate * inputs
+
+    def weight(self):
+        return self.w
+
+
+class SVM(Algorithm):
+    # initilizae the examples.
+    def setup(self, train_x, train_y):
+        tmp = np.loadtxt(train_x, dtype='str', delimiter=',')
+        listresult = Algorithm.checkRow(tmp)
+        # switch the letter of the gender.
+        for x in range(tmp.shape[0]):
+            tmp[x][0] = Algorithm.switch(tmp[x][0],listresult)
+        self.training_inputs = tmp.astype(float)
+        self.training_inputs = (self.training_inputs - np.mean(self.training_inputs, axis=0)) / np.std(
+            self.training_inputs, axis=0)
+        self.labels = np.loadtxt(train_y, dtype=int)
+        # shuffle examples
+        x = np.max(self.labels[0])
+        # set width
+        self.w = np.zeros((3, 8), dtype=float)
+        self.learning_rate = 0.1
+        self.lamda=2
+
+    def train(self):
+       # s = ""
+        count = 0
+        for set in range(50):
+            c = list(zip(self.training_inputs, self.labels))
+            shuffle(c)
+            self.training_inputs, self.labels = zip(*c)
+            for inputs, label in zip(self.training_inputs, self.labels):
+                y_hat = np.argmax(np.dot(self.w, inputs))
+                if (y_hat != label):
+                    self.w[label, :] =(1-self.lamda * self.learning_rate) *self.w[label,:] + self.learning_rate * inputs
+                    self.w[y_hat, :] =(1-self.lamda * self.learning_rate) *self.w[label,:] -self.learning_rate * inputs
+
+    def weight(self):
+        return self.w
+
+class PA(Algorithm):
+    # initilizae the examples.
+    def setup(self, train_x, train_y):
+        tmp = np.loadtxt(train_x, dtype='str', delimiter=',')
+        listresult = Algorithm.checkRow(tmp)
+        # switch the letter of the gender.
+        for x in range(tmp.shape[0]):
+            tmp[x][0] = Algorithm.switch(tmp[x][0],listresult)
+        self.training_inputs = tmp.astype(float)
+        if(np.std(self.training_inputs,axis=0) == 0):
+            print("zero")
+        self.training_inputs = (self.training_inputs - np.mean(self.training_inputs, axis=0)) / (2 * np.std(
+            self.training_inputs, axis=0) )
+        self.labels = np.loadtxt(train_y, dtype=int)
+        # shuffle examples
+        x = np.max(self.labels[0])
+        # set width
+        self.w = np.zeros((3, 8), dtype=float)
+        self.lamda=0
+
+    def train(self):
+        s = ""
+        count = 0
+        for set in range(50):
+            c = list(zip(self.training_inputs, self.labels))
+            shuffle(c)
+            self.training_inputs, self.labels = zip(*c)
+            for inputs, label in zip(self.training_inputs, self.labels):
+                y_hat = np.argmax(np.dot(self.w, inputs))
+                if (y_hat != label):
+                    self.lamda= max(0,1-np.dot( self.w[label],inputs) + np.dot( self.w[y_hat],inputs))\
+                                /(np.linalg.norm(inputs)**2)
+                    self.w[label, :] +=self.lamda *inputs
+                    self.w[y_hat, :] -=self.lamda *inputs
+                # printtothefile("result.txt","perceptron: " + str(y_hat) + "\n")
+                s += str(y_hat) + "\n"
+
+    def weight(self):
+        return self.w
+
+
+def main():
+    inputx=""
+    inputy=""
+    inputext=""
+    for arg in sys.argv[1:]:
+        if(arg=="train_x.txt"):
+            inputx=arg
+        if(arg== "train_y.txt"):
+            inputy=arg
+        if(arg == "test_x.txt"):
+            inputext=arg
+    count = Algorithm.file_len(inputx)
+    f = open("result.txt", "a")
+    # perceptron execution
+    perceptron = Perceptron()
+    perceptron.setup(inputx, inputy)
+    perceptron.train()
+    w= perceptron.weight()
+    per_values = perceptron.test(inputext,w)
+
+    svm = SVM()
+    svm.setup(inputx, inputy)
+    svm.train()
+    w = svm.weight()
+    svm_values = svm.test(inputext,w)
+
+    pa = PA()
+    pa.setup(inputx, inputy)
+    pa.train()
+    w = pa.weight()
+    pa_values = pa.test(inputext,w)
+    for i in range (count):
+        Algorithm.printtothefile(f,"perceptron: " + str(per_values[i]) + " SVM: " + str(svm_values[i]) +
+                                 " PA: " + str(pa_values[i]) + "\n")
+    f.close()
+    f= open("result.txt")
+    for line in f:
+        print(line)
+
+
+
+if __name__ == "__main__":
+    main()
